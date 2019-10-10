f9f5a1131e017872216c4c9fab8818e1626a8e7c
davidd11 <ddoan.0411@gmail.com>
2019-10-10 20:24:55 +1100

    add correct links to navbar

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -10,16 +10,10 @@ class Header extends Component {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Workouts</Nav.Link>
-            <Nav.Link href="#link">Log</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/workouts">Workouts</Nav.Link>
+            <Nav.Link href="/log">Log</Nav.Link>
+            <Nav.Link href="/nutrition">Nutrition</Nav.Link>
             </Nav>
             <Nav.Link href="/sign-up">Sign Up</Nav.Link>
             <Nav.Link href="/log-in">Log In</Nav.Link>
