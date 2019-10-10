ef41da6b1b3cb582efa15123a18520bf1cb375a3
davidd11 <ddoan.0411@gmail.com>
2019-10-10 20:57:29 +1100

    remove username, description and add title to exercise schema

--- backend/models/exercise.js
+++ backend/models/exercise.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 const exerciseSchema = new mongoose.Schema({
-    username: {type: String, required: true},
-    description: {type: String, required: true},
+    title: {type: String, required: true},
     reps: {type: Number, required: true },
     weight: {type: Number, required: true},
 }, {
