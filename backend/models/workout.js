589a079cff509fc829835adcb027af2ee5cb3c45
davidd11 <ddoan.0411@gmail.com>
2019-10-10 22:03:05 +1100

    add title to workout schema

--- backend/models/workout.js
+++ backend/models/workout.js
@@ -3,6 +3,10 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new mongoose.Schema({
     username: { type: Schema.Types.ObjectId, ref: 'User' },
-    date: {type: Date, required: true},
+    title: { type: String },
+    date: { type: Date, required: true },
     exercises: [{ type: Schema.Types.ObjectId, ref: 'Exercise' }]
 });
+
+
+module.exports = mongoose.model('Workout', workoutSchema);
