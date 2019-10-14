d5caccaf933e099c06efdb3409d885303a2ad039
davidd11 <ddoan.0411@gmail.com>
2019-10-14 16:18:32 +1100

    display exercise categories

--- frontend/src/components/exercises.jsx
+++ frontend/src/components/exercises.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Link from 'react-router-dom';
+import { ListGroup } from 'react-bootstrap';
 
 const h1 = {
     textAlign: "center",
@@ -9,18 +12,31 @@ class ExercisesPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            exercises: []
+            exerciseCategories: []
         }
     }
 
-    componentDidMount() {
-
+    componentWillMount() {
+        // const url = "https://wger.de/api/v2/exercise/?language=2&status=2";
+        const url = "https://wger.de/api/v2/exercisecategory/"
+        axios.get(url)
+        .then(res => {
+            console.log(res.data);
+            this.setState({
+                exerciseCategories: res.data.results
+            })
+        })
     }
 
     render() {
         return(
             <div className="container">
-                <h1 style={h1}>List of Exercises</h1>
+                <h1 style={h1}>Exercises</h1>
+                <ListGroup>
+                {this.state.exerciseCategories.map(category =>
+                    <ListGroup.Item action href={"/exercises/"+category.name}>{category.name}</ListGroup.Item>
+                )}
+                </ListGroup>
             </div>
         )
     }
