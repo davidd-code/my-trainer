3421e29dc5183bfebe97903e8d43e2248d5b9f89
davidd11 <ddoan.0411@gmail.com>
2019-10-14 22:19:23 +1100

    workout front end

---
+++ frontend/src/components/workoutList.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { Table, Button, ButtonToolbar } from 'react-bootstrap';
+
+const heading = {
+    textAlign: "center",
+    paddingTop: "2vh"
+}
+
+const button = {
+    marginTop: "4vh"
+}
+class WorkoutListPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return(
+            <div className="container">
+                <h2 style={heading}>Workouts</h2>
+                <ButtonToolbar>
+                    <Button href="/workouts/create/" style={button} variant="primary" block>Create Workout</Button>
+                </ButtonToolbar>
+            </div>
+        )
+    }
+}
+
+export default WorkoutListPage;
