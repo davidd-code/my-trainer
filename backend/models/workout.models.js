b426f6f09d9dc09ef762631d39eb32e2e8f66d0d
davidd11 <ddoan.0411@gmail.com>
2019-10-15 20:08:36 +1100

    update workout schema

--- backend/models/workout.models.js
+++ backend/models/workout.models.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const workoutSchema = new mongoose.Schema({
-    username: { type: Schema.Types.ObjectId, ref: 'User' },
-    title: { type: String },
+    skeleton: { type: Schema.Types.ObjectId, ref: 'Skeleton' },
     date: { type: Date, required: true },
-    exercises: [{ type: Schema.Types.ObjectId, ref: 'Exercise' }]
 });
 
 
