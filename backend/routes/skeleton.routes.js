1dc0065bfafa061105461a9873c18516b55116bf
davidd11 <ddoan.0411@gmail.com>
2019-10-15 19:37:11 +1100

    create skeleton routes

---
+++ backend/routes/skeleton.routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+let Skeleton = require('../models/skeleton.models');
+
+router.route('/').get((req, res) =>{
+    Skeleton.find()
+    .then(skeleton => res.json(skeleton))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) =>{
+    const title = req.body.title;
+    const exercises = req.body.exercises;
+
+    const newSkeleton = new Skeleton({
+        title,
+        exercises,
+    });
+
+    newSkeleton.save()
+        .then(() => res.json('Skeleton added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
